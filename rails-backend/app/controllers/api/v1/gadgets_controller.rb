d428921fe6a1f99a8affd9619d0d163b68b050f1
ocalvario <oswald.calvario@gmail.com>
2020-08-08 23:51:59 -0400

    Finishes gadget controller

--- rails-backend/app/controllers/api/v1/gadgets_controller.rb
+++ rails-backend/app/controllers/api/v1/gadgets_controller.rb
@@ -1,31 +1,32 @@
 class Api::V1::GadgetsController < ApplicationController
 
-   def index
-        @gadgets = Gadget.all
+   before_action :set_cat 
+
+    def index
+        @gadgets = @cat.gadgets
         render json: @gadgets
-   end
+    end
+
+    def show
+        @gadget = @cat.gadgets.find_by(id: params[:id])
+        @gadget = Gadget.find(params[:id])
+        render json: @gadget
+    end 
 
    def create
-    @gadget = Gadget.new(gadget_params)
-    if @gadget.save
-        render json: @gadget
-    else
-        render json: {error: 'Error creating gadget'}
-    end
+
    end
 
-   def show
-    @gadget = Gadget.find(params[:id])
-    render json: @gadget
-   end 
-
    def destroy
-    @gadget = Gadget.find(params[:id])
-    @gadget.destroy
+
    end 
 
    private
 
+   def set_cat
+    @cat = Cat.find(params[:cat_id])
+   end 
+   
    def gadget_params
     params.require(:gadget).permit(:cat_id, :name, :image_url, :description, :link)
    end
