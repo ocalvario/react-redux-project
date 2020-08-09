27cb13d634e3b79e4bdb8c86215976581c15d1a7
ocalvario <oswald.calvario@gmail.com>
2020-08-08 22:17:50 -0400

    Adds index and create actions for gadgets

--- rails-backend/app/controllers/api/v1/gadgets_controller.rb
+++ rails-backend/app/controllers/api/v1/gadgets_controller.rb
@@ -1,2 +1,31 @@
 class Api::V1::GadgetsController < ApplicationController
+
+   def index
+        @gadgets = Gadget.all
+        render json: @gadgets
+   end
+
+   def create
+    @gadget = Gadget.new(gadget_params)
+    if @gadget.save
+        render json: @gadget
+    else
+        render json: {error: 'Error creating account'}
+    end
+   end
+
+   def show
+
+   end 
+
+   def destroy
+
+   end 
+
+   private
+
+   def gadget_params
+    params.require(:gadget).permit(:cat_id, :name, :image_url, :description, :link)
+   end
+
 end
