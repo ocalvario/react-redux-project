f42f88af80ae9bde568507d53b34f2a5ae03f0d5
ocalvario <oswald.calvario@gmail.com>
2020-08-13 22:22:54 -0400

    Adds form to cat input

--- react-frontend/src/components/CatInput.js
+++ react-frontend/src/components/CatInput.js
@@ -6,7 +6,17 @@ class CatInput extends React.Component {
     render() {
         return (
             <div>
-                CatInput   
+                <form>
+                   <label>Name: </label>
+                   <input type="text" placeholder="Name"/> <br></br>
+                   <label>Image Url: </label>
+                   <input type="text" placeholder="Image Url"/> <br></br>
+                   <label>Description: </label>
+                   <input type="text" placeholder="Description"/> <br></br>
+                   <label>Link: </label>
+                   <input type="text" placeholder="Link"/> <br></br>
+                </form>  
+                <br></br>
             </div>
         )
     }
