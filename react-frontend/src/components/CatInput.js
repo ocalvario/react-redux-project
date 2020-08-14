ef4d75db3ba81eb8d7d7a06bd816fcecde4e8270
ocalvario <oswald.calvario@gmail.com>
2020-08-13 23:37:04 -0400

    State integrated into form

--- react-frontend/src/components/CatInput.js
+++ react-frontend/src/components/CatInput.js
@@ -3,20 +3,24 @@ import React from 'react'
 
 class CatInput extends React.Component {
 
-    state = { name: '', im,age_url: '', description: '', link: ''}
+    state = { name: '', image_url: '', country: ''}
+
+    handleChange = (event) => {
+        this.setState({
+          [event.target.name]: event.target.value 
+        })
+    }
 
     render() {
         return (
             <div>
                 <form>
                    <label>Name: </label>
-                   <input type="text" placeholder="Name"/> <br></br>
+                   <input type="text" placeholder="Name" value={this.state.name} name="name" onChange ={this.handleChange} /> <br></br>
                    <label>Image Url: </label>
-                   <input type="text" placeholder="Image Url"/> <br></br>
+                   <input type="text" placeholder="Image Url" value={this.state.image_url} name="image_url" onChange ={this.handleChange} /> <br></br>
                    <label>Description: </label>
-                   <input type="text" placeholder="Description"/> <br></br>
-                   <label>Link: </label>
-                   <input type="text" placeholder="Link"/> <br></br>
+                   <input type="text" placeholder="Country" value={this.state.country} name="country" onChange ={this.handleChange} /> <br></br>
                 </form>  
                 <br></br>
             </div>
