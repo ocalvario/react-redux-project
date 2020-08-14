09e439e42e702697564bad2f1a87af042d26cfcd
ocalvario <oswald.calvario@gmail.com>
2020-08-14 19:04:20 -0400

    Processes fetch post data

--- react-frontend/src/reducers/catReducer.js
+++ react-frontend/src/reducers/catReducer.js
@@ -3,6 +3,8 @@ export default function catReducer(state = {cats: []}, action) {
     switch (action.type) {
      case 'FETCH_CATS':
          return {cats: action.payload}
+    case 'ADD_CAT':
+        return {...state, cats: [...state.cats, action.payload] }
      default:
          return state
     }
