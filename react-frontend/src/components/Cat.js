fb4919869d2e137a8278707671f733d86faa5f00
ocalvario <oswald.calvario@gmail.com>
2020-09-13 14:36:32 -0400

    Sets up and links gadget container

--- react-frontend/src/components/Cat.js
+++ react-frontend/src/components/Cat.js
@@ -1,14 +1,19 @@
  import React from 'react'
  import {Redirect} from 'react-router-dom'
 
+ import GadgetsContainer from '../containers/GadgetsContainer'
+
  const Cat = (props) => {
 
     let cat = props.cats[props.match.params.id - 1]
 
     return (
-        <h2>
-            {cat ? cat.name : null} - {cat ? cat.country : null} 
-        </h2>
+        <div>
+            <h2>
+                {cat ? cat.name : null} - {cat ? cat.country : null} 
+            </h2>
+            <GadgetsContainer/>
+        </div>
         
     )
 
---
+++ react-frontend/src/containers/GadgetsContainer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+class GadgetsContainer extends React.Component {
+
+render() {
+    return (
+        <div>
+            GadgetsContainer
+        </div>
+
+    )
+
+
+}
+
+}
+
+export default GadgetsContainer
