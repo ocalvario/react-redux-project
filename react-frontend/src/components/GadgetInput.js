61d4740765d65ea2e6778ef9718751d128de31ee
ocalvario <oswald.calvario@gmail.com>
2020-09-13 17:13:07 -0400

    Adds form to gadget input

--- react-frontend/src/components/GadgetInput.js
+++ react-frontend/src/components/GadgetInput.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {connect} from 'react-redux'
 
 
 class GadgetInput extends React.Component {
@@ -6,7 +7,17 @@ class GadgetInput extends React.Component {
     render() {
         return (
             <div>
-                GadgetInput
+                <form>
+                    <label>Name:</label>
+                    <input type="text" name="name" value={this.state.name} onChange={this.handleChange} /> <br></br>
+                    <label>Image:</label>   
+                    <input type="text" name="image_url" value={this.state.image_url} onChange={this.handleChange} /> <br></br>
+                    <label>Description:</label>
+                    <input type="text" name="description" value={this.state.description} onChange={this.handleChange} /> <br></br>
+                    <label>Link:</label>
+                    <input type="text" name="link" value={this.state.link} onChange={this.handleChange} /> <br></br>
+                    <br></br>
+                </form>
             </div>
         )
 
@@ -14,4 +25,4 @@ class GadgetInput extends React.Component {
 
 }
 
-export default GadgetInput
+export default connect(null)(GadgetInput)
