8878812d8e1aeeac3e7b6ed3b47ed203dd2e6a7d
ocalvario <oswald.calvario@gmail.com>
2020-10-15 14:26:27 -0400

    Votes only update on button clicks

--- react-frontend/src/components/RaitingButton.js
+++ react-frontend/src/components/RaitingButton.js
@@ -8,22 +8,19 @@ class RaitingButton extends React.Component {
         window.addEventListener("click", this.handleMouseClick);
         }
 
-        componentWillUnmount() {
-        window.removeEventListener("click", this.handleMouseClick);
-        }
-
         handleMouseClick = (e) => {
             if (String(this.props.id) === e.target.id)
             {this.setState({count: 1})}
-            else {this.setState ({count: 0})}          
-
+            else if (e.target.name === "vote")
+             {this.setState ({count: 0})} 
+            else {}         
         }
      
 
     render () {
         return (
                 
-                <button id={this.props.id}> Votes: {this.state.count} </button>
+                <button name="vote" id={this.props.id}> Votes: {this.state.count} </button>
         
         )
         }
